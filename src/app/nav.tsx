91cd2665d2930773712f32e60f3bb13a82102de0
shannonsia <shannonmcsia@gmail.com>
2023-06-01 01:50:20 +1000

    final edits

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -30,7 +30,29 @@ function Navbar() {
           <img src="/images/account.png" className={styles.accountIcon}></img>
         </div>
         <div className={styles.navRight}>
-          <div className={styles.navRedBar}>HELLO</div>
+          <div className={styles.navRedBar}>
+            <div className={styles.header}>
+              <div className={styles.stats}>
+                <div className={styles.pokecoinContainer}>
+                  <img
+                    src="/images/pokecoin.png"
+                    className={styles.pokecoin}
+                  ></img>{" "}
+                  Pokecoin
+                </div>
+                <div className={styles.pokeballContainer}>
+                  <img
+                    src="/images/pokeball.png"
+                    className={styles.pokeball}
+                  ></img>{" "}
+                  Pokeball
+                </div>
+              </div>
+            </div>
+            <div className={styles.weather}>
+              <img src="/images/umbrella.png" className={styles.umbrella}></img>
+            </div>
+          </div>
           <div className={styles.navBar}>
             <div className={styles.navItemsContainer}>
               <div className={styles.navItems}>
@@ -53,6 +75,17 @@ function Navbar() {
                 {navItems.map((item) => (
                   <div key={item} className={styles.navItem}>
                     {item}
+                    <svg
+                      className={styles.arrow}
+                      viewBox="0 0 39 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M28.1171 0.117249L26.288 1.94639L34.0482 9.70666H0.318848V9.70674V12.2935V12.2936H34.048L26.288 20.0536L28.1171 21.8827L39 11L28.1171 0.117249Z"
+                        fill="#BBBBBB"
+                      />
+                    </svg>
                   </div>
                 ))}
               </div>
