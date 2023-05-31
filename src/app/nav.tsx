ba806a3b3d5ebae28f55a51b2b7af3847c6ba7db
shannonsia <shannonmcsia@gmail.com>
2023-05-31 22:45:24 +1000

    icons

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -6,21 +6,25 @@ function Navbar() {
     const navItems = ["Pokemon Lab", "Battle", "Poke Mart", "My Pokedex"]
 return (
     <>
-    <div className={styles.navcontainer}>
-    <img src="/public/logo.jpg"></img>
-    <div className={styles.navright}>
-    <div className={styles.navredbar}>HELLO</div>
-    <div className={styles.navbar}>
-        <div className={styles.navitemscontainer}>
-            <div className={styles.navitems}>
+    <div className={styles.navContainer}>
+        <div className={styles.icons}>
+    <img src="/images/hamburger.png" className={styles.hamburgerIcon}></img>
+    <img src="/images/logo.png" className={styles.logoIcon}></img>
+    <img src="/images/account.png" className={styles.accountIcon}></img>
+    </div>
+    <div className={styles.navRight}>
+    <div className={styles.navRedBar}>HELLO</div>
+    <div className={styles.navBar}>
+        <div className={styles.navItemsContainer}>
+            <div className={styles.navItems}>
           {navItems.map((item) => (
-            <div key={item} className={styles.navitem}>
+            <div key={item} className={styles.navItem}>
               {item}
             </div>
           ))}
           </div>
           </div>
-              <button className={styles.trainerbutton}>Become a Trainer</button>
+              <button className={styles.trainerButton}>Become a Trainer</button>
         </div>
         </div>
         </div>
