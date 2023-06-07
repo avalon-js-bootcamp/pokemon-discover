605114dc90ef6025d0009ef5e8d22d5560e26576
shannonsia <shannonmcsia@gmail.com>
2023-06-08 00:29:19 +1000

    fixed close button/mobile nav display

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -18,14 +18,16 @@ function Navbar() {
             }}
             style={{ display: isNavExpanded ? "none" : "block" }}
           ></img>
-          <img
-            src="/images/close.png"
-            className={styles.closeIcon}
-            onClick={() => {
-              setIsNavExpanded(!isNavExpanded);
-            }}
-            style={{ display: isNavExpanded ? "block" : "none" }}
-          ></img>
+          <div className={styles.closeIconContainer}>
+            <img
+              src="/images/close.png"
+              className={styles.closeIcon}
+              onClick={() => {
+                setIsNavExpanded(!isNavExpanded);
+              }}
+              style={{ display: isNavExpanded ? "block" : "none" }}
+            ></img>
+          </div>
           <img src="/images/logo.png" className={styles.logoIcon}></img>
           <img src="/images/account.png" className={styles.accountIcon}></img>
         </div>
@@ -69,31 +71,33 @@ function Navbar() {
             <button className={styles.trainerButton}>Become a Trainer</button>
           </div>
 
-          <div
-            className={styles.navMobileBar}
-            style={{ display: isNavExpanded ? "flex" : "none" }}
-          >
-            <div className={styles.navItemsContainer}>
-              <div className={styles.navItems}>
-                {navItems.map((item) => (
-                  <div key={item} className={styles.navItem}>
-                    {item}
-                    <svg
-                      className={styles.arrow}
-                      viewBox="0 0 39 22"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M28.1171 0.117249L26.288 1.94639L34.0482 9.70666H0.318848V9.70674V12.2935V12.2936H34.048L26.288 20.0536L28.1171 21.8827L39 11L28.1171 0.117249Z"
-                        fill="#BBBBBB"
-                      />
-                    </svg>
-                  </div>
-                ))}
+          <div className={styles.navMobileContainer}>
+            <div
+              className={styles.navMobileBar}
+              style={{ display: isNavExpanded ? "flex" : "none" }}
+            >
+              <div className={styles.navItemsContainer}>
+                <div className={styles.navItems}>
+                  {navItems.map((item) => (
+                    <div key={item} className={styles.navItem}>
+                      {item}
+                      <svg
+                        className={styles.arrow}
+                        viewBox="0 0 39 22"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M28.1171 0.117249L26.288 1.94639L34.0482 9.70666H0.318848V9.70674V12.2935V12.2936H34.048L26.288 20.0536L28.1171 21.8827L39 11L28.1171 0.117249Z"
+                          fill="#BBBBBB"
+                        />
+                      </svg>
+                    </div>
+                  ))}
+                </div>
               </div>
+              <button className={styles.trainerButton}>Become a Trainer</button>
             </div>
-            <button className={styles.trainerButton}>Become a Trainer</button>
           </div>
         </div>
       </div>
