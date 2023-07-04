24b80fa1a9445e13ffb0c1aa3c55e25db53d827e
xBunnicornx <angieeee.lnmt@gmail.com>
2023-07-04 23:44:02 +1000

    fix responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-
 import "./components/page.css";
 import Picture from "./components/picture";
 export default function Home() {
   return (
     <div className="mainPage">
       <Picture />
-
     </div>
   );
 }
